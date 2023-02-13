cf16814ad9b21ffaaf528d5266d36202daaba313
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2023-02-13 18:48:45 -0500

    added new database

--- LastSurvivalKit/settings.py
+++ LastSurvivalKit/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +80,11 @@ WSGI_APPLICATION = 'LastSurvivalKit.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Last_Survival_kit',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD':os.getenv('PASSWORD'),
+        'HOST':os.getenv('HOST'),
+        'PORT':os.getenv('PORT'),
     }
 }
 
