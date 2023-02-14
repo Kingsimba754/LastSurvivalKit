5be4e68f174a771bba94236e4ed4b71151be64df
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2023-02-13 20:16:04 -0500

    added white noise

--- LastSurvivalKit/settings.py
+++ LastSurvivalKit/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,7 +132,7 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = '/people/'
 
 LOGOUT_REDIRECT_URL =  '/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
